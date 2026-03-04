c5eb88dc5ff704a755233b2e173f1cab72c1b503
Julio Pulido <juliopuli@gmail.com>
2026-03-04 12:12:29 +0100

    V.6.1.0: Security fixes and code cleanup. Only authorized files included.

--- test_firestore.ps1
+++
@@ -1,2 +0,0 @@
-$response = Invoke-RestMethod -Uri "https://firestore.googleapis.com/v1/projects/emercre/databases/(default)/documents/eries?pageSize=5"
-$response.documents.fields | ConvertTo-Json -Depth 10 | Out-File -FilePath .\eries_dump.json -Encoding utf8
