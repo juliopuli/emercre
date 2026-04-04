75f9a948c9acec3b7d3cc4ca3b8d1c7c55805903
Julio Pulido <juliopuli@gmail.com>
2026-04-04 16:03:56 +0200

    chore: Final cleanup of prohibited files and version synchronization to V.15.11.0

--- scripts/queryA2910.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin');
-const fs = require('fs');
-
-async function main() {
-    process.env.GOOGLE_APPLICATION_CREDENTIALS = '/home/julio/Documentos/Antigravity/EmerCRE/firebase.json'; // not right. 
-    // We can run this script using `firebase functions:shell` or by initializing admin with default credentials if logged in via gcloud.
-    // Or we can just read the Oysta Bridge API directly if we know A-29.10's ID.
-}
-
